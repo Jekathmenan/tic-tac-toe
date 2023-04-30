11a181a23954a137e0c6bfe233c4df1e590d78d5
Jekathmenan Selvarajah <jekath27@gmail.com>
2023-04-30 18:12:54 +0200

    Bugfixes and doc

--- src/Controller/TicTacToeController.php
+++ src/Controller/TicTacToeController.php
@@ -10,56 +10,64 @@ class TicTacToeController extends AbstractController {
     private $gameController;
     
     public function index(Request $request): Response {
-        
-        if ($request->isMethod('POST')) {
-            $board = $request->request->get('board');
-            $board = json_decode($board, true);
-        }
+        return $this->render('menu/menu.html.twig');
+    }
+
+    /**
+     * Initializes GameController with the required Values and redirects to Play Route
+     */
+    public function initGame(Request $request): Response {
         
         $this->gameController = new GameController();
-        
-        $board = $this->gameController->getBoard();
-        $currentPlayer = $this->gameController->getCurrentPlayer();
-        $winner = $this->gameController->getWinner();
+
+        if (isset($_POST['playertype']) && $_POST['playertype'] === 'pvp') {
+            $this->gameController->setPVP(true);
+        } else {
+            $this->gameController->setPVP(false);
+        }
 
         // Storing game data to Session
         $session = $request->getSession();
         $this->gameController->storeGameData($session);
         
-        return $this->render('tictactoe/play.html.twig', [
-            'board' => $board,
-            'currentPlayer' => $currentPlayer,
-            'winner' => $winner
-        ]);
+        return $this->redirectToRoute('tic_tac_toe.game');
     }
 
-    public function play(Request $request) {
+    /**
+     * Plays game and renders Gameboard with the new Values
+     */
+    public function play(Request $request):Response {
+        // Restoring GameController from Session
         $session = $request->getSession();
         $this->gameController = new GameController();
         $this->gameController->restoreGameData($session);
         
+        // Reading Users move
         $arr = [];
-        
         foreach($_POST as $key => $val) {
             $arr = explode(",", $key);
         }
 
+        // Playing users next Move
+        $winner = $this->gameController->getWinner();
+        if (!empty($arr)) {
+            $winner = $this->gameController->playNextMove($arr[0], $arr[1]);
+        }
         
-        
-        $winner = $this->gameController->playNextMove($arr[0], $arr[1]);
+        // Initializing variables to bind to View
         $board = $this->gameController->getBoard();
         $currentPlayer = $this->gameController->getCurrentPlayer();
+        $pvp = $this->gameController->getPVP();
 
-        if ($winner === 'X' || $winner === 'O') {
-            $this->addFlash('game-end', 'Spiel ist zu Ende. Spieler ' . $winner . ' hat gewonnen.');
-        }
-
+        // Storing GameController to Session
         $this->gameController->storeGameData($session);
 
+        // Binding required Variables to View and returning the View
         return $this->render('tictactoe/play.html.twig', [
             'board' => $board,
             'currentPlayer' => $currentPlayer,
-            'winner' => $winner
+            'winner' => $winner,
+            'pvp' => $pvp
         ]);
     }
 }
